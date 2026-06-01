31e0705a2552c8688a3d48f45df7a03f1b10666d
macielnassar <macielnassarm@gmail.com>
2026-05-31 21:36:30 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+const products = [
+  // 🍰 CHEESECAKES
+  {
+    name: "Classic Cheesecake",
+    price: 25,
+    cat: "cheesecakes",
+    img: "https://share.google/DT5kAfatppF95W4Xq"
+  },
+  {
+    name: "Chocolate Cheesecake",
+    price: 28,
+    cat: "cheesecakes",
+    img: "https://share.google/BkimwujDtq5sAYieM"
+  },
+  {
+    name: "Oreo Cheesecake",
+    price: 30,
+    cat: "cheesecakes",
+    img: "https://share.google/VPGPOyDxb0NeUJeu1"
+  },
+  {
+    name: "Red Berries Cheesecake",
+    price: 30,
+    cat: "cheesecakes",
+    img: "https://share.google/76zKTU7iU7PYwTEd8"
+  },
+  {
+    name: "Pistachio Cheesecake",
+    price: 32,
+    cat: "cheesecakes",
+    img: "https://share.google/pLh16YVBvCYpeV7t4"
+  },
+  {
+    name: "Passion Fruit Cheesecake",
+    price: 32,
+    cat: "cheesecakes",
+    img: "https://share.google/wgvvt4X0w70EBmoGx"
+  },
+
+  // 🍮 OTHER DESSERTS
+  {
+    name: "Tiramisu",
+    price: 30,
+    cat: "others",
+    img: "https://share.google/W9Qfjjf4twIEGsAUa"
+  },
+  {
+    name: "Pies",
+    price: 28,
+    cat: "others",
+    img: "https://share.google/7lu1eE6hUchY9u24H"
+  },
+  {
+    name: "Pavlova",
+    price: 28,
+    cat: "others",
+    img: "https://share.google/u0YVfVKkR3gFF9nzD"
+  },
+
+  // ☕ DRINKS
+  {
+    name: "Coffee",
+    price: 20,
+    cat: "drinks",
+    img: "https://share.google/1NKgtF4FHmOK9g0O6"
+  },
+  {
+    name: "Iced Drinks",
+    price: 18,
+    cat: "drinks",
+    img: "https://share.google/RkWIXtTtDCVJmU9SR"
+  }
+];
